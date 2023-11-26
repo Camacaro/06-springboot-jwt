83b164dd952b0399fac28061663e8ce0add3a8ae
Jesus Camacaro <jesus.alejandr@gmail.com>
2023-11-26 10:20:45 -0300

    init

---
+++ src/main/java/com/jesus/courses/springboot/jwtauthenticationserver/app/SpringbootJwtAuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.courses.springboot.jwtauthenticationserver.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtAuthenticationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtAuthenticationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jesus/courses/springboot/jwtauthenticationserver/app/SpringbootJwtAuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.courses.springboot.jwtauthenticationserver.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtAuthenticationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
